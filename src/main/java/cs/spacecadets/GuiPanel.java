32e15496f04c65702efd1e1c7b7f20036b1c08fb
Andrew Milne <andrew@milne.me.uk>
2023-11-16 22:38:54 +0000

    2023-11-16 22:34
    Overhauled everything as progression towards displaying every image continues

--- src/main/java/cs/spacecadets/GuiPanel.java
+++ src/main/java/cs/spacecadets/GuiPanel.java
@@ -1,10 +1,10 @@
 package cs.spacecadets;
+
 import javax.swing.*;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
 
-public class GuiPanel extends JPanel {
+public class GuiPanel extends JLabel {
     private final int operator;
 
     public GuiPanel(int operator) {
@@ -13,15 +13,15 @@ public class GuiPanel extends JPanel {
     }
     
     public void showImage(BufferedImage image) {
-        BufferedImage imgGraphics;
-        if (operator == 1) {imgGraphics = new BufferedImage(image.getWidth(),image.getHeight(),BufferedImage.TYPE_BYTE_GRAY);}
-        else if (operator == 2) {imgGraphics = sobel(image,0);}
-        else if (operator == 3) {imgGraphics = sobel(image,1);}
-        else if (operator == 4) {imgGraphics = sobel(image,2);}
-        else {imgGraphics = image;}
-        Graphics graphics = imgGraphics.getGraphics();
-        graphics.drawImage(image,0,0,null);
-        paintComponent(graphics);
+        BufferedImage imgGraphics = switch (operator) {
+            case 1 -> new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+            case 2 -> sobel(image, 0);
+            case 3 -> sobel(image, 1);
+            case 4 -> sobel(image, 2);
+            default -> image;
+        };
+        ImageIcon icon = new ImageIcon(imgGraphics);
+        this.setIcon(icon);
     }
     
     private BufferedImage sobel(BufferedImage image, int axis) {
@@ -36,24 +36,38 @@ public class GuiPanel extends JPanel {
         int finalA;
         int finalB;
         
-        BufferedImage newImage = new BufferedImage(image.getWidth(),image.getHeight(),image.getType());
+        BufferedImage newImage = new BufferedImage(image.getWidth(),image.getHeight(),BufferedImage.TYPE_INT_ARGB);
         
         for(int x=0; x<image.getWidth(); x++) {
-            for(int y=0; y<image.getHeight(); y++) {
-                xVal=0;
-                yVal=0;
-                for(int a=-1; a<2; a++) {
-                    for(int b=-1; b<2; b++) {
-                        if(a+x<0) {finalA=0;} else finalA = Math.min(a+x, image.getWidth()); //Extend image horizontally 1 pixel on each side
-                        if(b+y<0) {finalB=0;} else finalB = Math.min(b+y, image.getWidth()); //Extend image vertically 1 pixel on each side
-                        if(axis==0 || axis==2) {xVal+=(image.getRGB(finalA,finalB)*xMap1.get(a+1)*xMap2.get(b+1));} //Sobel operator on x
-                        if(axis==1 || axis==2) {yVal+=(image.getRGB(finalA,finalB)*yMap1.get(a+1)*yMap2.get(b+1));} //Sobel operator on y
+            for(int y=0; y<image.getHeight(); y++) { //(x,y) is the co-ordinates of the pixel in the image we are looking at
+                xVal = 0;
+                yVal = 0;
+                for (int a = -1; a < 2; a++) {
+                    for (int b = -1; b < 2; b++) {
+                        if (a + x < 0) {
+                            finalA = 0;
+                        } else {
+                            finalA = Math.min(a + x, image.getWidth()-1); //Extend image horizontally 1 pixel on each side
+                        }
+                        if (b + y < 0) {
+                            finalB = 0;
+                        } else {
+                            finalB = Math.min(b + y, image.getHeight()-1); //Extend image vertically 1 pixel on each side
+                        }
+                        if (axis == 0 || axis == 2) {
+                            xVal += (image.getRGB(finalA, finalB) * xMap1.get(a + 1) * xMap2.get(b + 1));
+                        } //Sobel operator on x
+                        if (axis == 1 || axis == 2) {
+                            yVal += (image.getRGB(finalA, finalB) * yMap1.get(a + 1) * yMap2.get(b + 1));
+                        } //Sobel operator on y
                     }
                 }
-                if(axis==0) {newVal = xVal;}
-                else if(axis==1) {newVal = yVal;}
-                else if(axis==2) {newVal = (int) Math.sqrt(Math.pow(xVal,2)+Math.pow(yVal,2));} //Gradient Magnitude
-                else{newVal=0;}
+                newVal = switch (axis) {
+                    case 0 -> xVal;
+                    case 1 -> yVal;
+                    case 2 -> getGradientMagnitude(xVal,yVal); //Gradient Magnitude
+                    default -> 0;
+                };
                 newImage.setRGB(x,y,newVal);
             }
         }
@@ -67,5 +81,22 @@ public class GuiPanel extends JPanel {
         map.put(1,y);
         map.put(2,z);
         return map;
+    }
+    
+    private int getGradientMagnitude(int redX, int redY) {
+        int greenX = redX%(256)-redX;
+        int blueX = redX%(256*256)-greenX-redX;
+        int alphaX = redX%(256*256*256)-blueX-greenX-redX;
+        
+        int greenY = redY%(256)-redY;
+        int blueY = redY%(256*256)-greenY-redY;
+        int alphaY = redY%(256*256*256)-blueY-greenY-redY;
+        
+        int finalRed = (int) Math.sqrt(Math.pow(redX, 2) + Math.pow(redY, 2));
+        int finalGreen = (int) Math.sqrt(Math.pow(greenX, 2) + Math.pow(greenY, 2));
+        int finalBlue = (int) Math.sqrt(Math.pow(blueX, 2) + Math.pow(blueY, 2));
+        int finalAlpha = (int) Math.sqrt(Math.pow(alphaX, 2) + Math.pow(alphaY, 2));
+
+        return finalRed+finalGreen+finalBlue+finalAlpha;
     }
 }
--- src/main/java/cs/spacecadets/Main.java
+++ src/main/java/cs/spacecadets/Main.java
@@ -1,32 +1,56 @@
 package cs.spacecadets;
-
 import com.github.sarxos.webcam.Webcam;
-import org.bytedeco.javacv.*;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) throws FrameGrabber.Exception {
-        FrameGrabber frameGrabber = new OpenCVFrameGrabber(0); //Default camera is 0
-        frameGrabber.start();
+    public static void main(String[] args) {
         Webcam camera = Webcam.getDefault();
         camera.open();
 
-        JFrame window = new JFrame("Circle Detector");
-        HashMap<Integer,GuiPanel> panelMap = new HashMap<>();
-        for (int i=0; i<6; i++) {
-            GuiPanel panel = new GuiPanel(i);
-            window.add(panel);
-            panel.setSize(300,300);
-            panel.setLocation(300*(i/2),300*(i%2));
-            panelMap.put(i,panel);
-        }
+        JFrame frame = new JFrame("Circle Detector");
+        Container bigPanel = frame.getContentPane();
+        bigPanel.setLayout(new BoxLayout(bigPanel, BoxLayout.Y_AXIS));
+        frame.setSize(900,600);
+        frame.setResizable(true);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        
+        Container leftPanel = frame.getContentPane();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.X_AXIS));
+        Container rightPanel = frame.getContentPane();
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.X_AXIS));
+        
+        HashMap<Integer, GuiPanel> panelMap = new HashMap<>();
+        addPanels(0, panelMap, leftPanel);
+        addPanels(3, panelMap, rightPanel);
         
         BufferedImage image = camera.getImage();
-        for (int i=0; i<6; i++) {
+        for (int i = 0; i < 6; i++) {
             panelMap.get(i).showImage(image);
+        }
+        
+        frame.setVisible(true);
+    }
+    
+    private static void addPanels(int add, HashMap<Integer, GuiPanel> panelMap, Container jPanel) {
+        for (int j = 0; j < 3; j++) {
+            int i = j+add;
+            GuiPanel panel = new GuiPanel(i);
+            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+            panel.setSize(300, 300);
+            panel.setLocation(300 * (i/2), 300 * (i%2));
+            panelMap.put(i, panel);
+            jPanel.add(panel);
         }
     }
 }
